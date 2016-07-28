72b3da87f542caddd1a828602907b7dedc062559
Maldon <maldonmeehan@gmail.com>
2016-07-28 07:33:59 -0700

    add npm init and install

--- js/cli.js
+++ js/cli.js
@@ -1,5 +1,7 @@
-console.log('Ping-ponging up to 100!');
-var result = pingPong(100);
+var pingPong = require('./ping-pong.js').pingPong;
+
+var goal = prompt('What number would you like me to ping-pong up to?');
+var result = pingPong(goal);
 result.forEach(function(element) {
   console.log(element);
 });
