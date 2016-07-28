2cbddb968da4b45465d93f546262bc0cae83460a
Maldon <maldonmeehan@gmail.com>
2016-07-28 16:44:22 -0700

    add Browserify with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
-gulp.task('myTask', function(){
-  console.log('hello gulp');
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/browser.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
