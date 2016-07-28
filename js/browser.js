c3b878653a5b3da0367e8ebf1557338c61794d8d
Maldon <maldonmeehan@gmail.com>
2016-07-28 07:15:48 -0700

    add function code from browser to ping-pong and add export to the function to create a module

--- js/browser.js
+++ js/browser.js
@@ -1,19 +1,3 @@
-function pingPong(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 == 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else  {
-      output.push(i);
-    }
-  }
-  return output;
-}
-
 $(document).ready(function(){
   $('#ping-pong').submit(function(event){
     event.preventDefault();
