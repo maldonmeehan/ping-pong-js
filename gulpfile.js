dedbe8b444611f041b552af7419ffe5671a20e39
Maldon <maldonmeehan@gmail.com>
2016-07-29 06:19:17 -0700

    add gulp build and clean tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var buildProduction = utilities.env.production;
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -21,4 +24,16 @@ gulp.task('minifyScripts', ['jsBrowserify'], function(){
   return gulp.src('./build/js/app.js')
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
