6e68a94d83205f5b878d9689b573df677e7c8f70
Maldon <maldonmeehan@gmail.com>
2016-07-29 05:58:27 -0700

    add minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -13,5 +14,11 @@ gulp.task('jsBrowserify', ['concatInterface'] , function() {
   return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src('./build/js/app.js')
+    .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
 });
